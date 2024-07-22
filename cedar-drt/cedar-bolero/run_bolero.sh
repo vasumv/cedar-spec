23ae5623ebfa360d78efd0cc8c02f5e0eb9dbc78
Vasu Vikram <vasumvik@amazon.com>
2024-07-22 19:16:03 +0000

    Add log to echo

--- cedar-drt/cedar-bolero/run_bolero.sh
+++ cedar-drt/cedar-bolero/run_bolero.sh
@@ -33,6 +33,6 @@ if [ "$BENCHMARK" == "raw_convert_policy_est_to_cedar"  ]; then
 fi
 
 source ../set_env_vars.sh
-echo 'cargo bolero test $BENCHMARK --corpus-dir $CORPUS_DIR --crashes-dir $CRASHES_DIR --engine $CONFIG -T $TIME -t 30s --jobs 1 --engine-args="-rss_limit_mb=0 -print_final_stats=1 -ignore_crashes=1"'
+echo 'cargo bolero test $BENCHMARK --corpus-dir $CORPUS_DIR --crashes-dir $CRASHES_DIR --engine $CONFIG -T $TIME -t 30s --jobs 1 --engine-args="-rss_limit_mb=0 -print_final_stats=1 -ignore_crashes=1 -fork=1"'
 cargo bolero test $BENCHMARK --corpus-dir $CORPUS_DIR --crashes-dir $CRASHES_DIR --engine $CONFIG -T $TIME -t 30s --jobs 1 --engine-args="-rss_limit_mb=0 -print_final_stats=1 -ignore_crashes=1 -fork=1"
 cp fuzz-0.log $RESULTS_DIR/fuzz.log
